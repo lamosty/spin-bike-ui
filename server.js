e317b5e02e7a1ae069f87269f852178dd12c8120
Rastislav Lamos <lamos.rasto@gmail.com>
2016-02-05 21:27:49 +0100

    Add boilerplate by @christianalfoni
    Instead of coming with a custom base project structure, I decided to use
    a simple webpack-express-react boilerplate created by Christian Alfoni.

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var express = require('express');
-var webpackConfig = require('../webpack.config');
-var path = require('path');
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
 
-var PORT = 3000;
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3000 : process.env.PORT;
+const app = express();
 
-var app = new express();
-var compiler = webpack(webpackConfig);
-
-app.use(
-	webpackDevMiddleware(
-		compiler, {
-			noInfo: true,
-			publicPath: webpackConfig.output.publicPath
+if (isDeveloping) {
+	const compiler = webpack(config);
+	const middleware = webpackMiddleware(compiler, {
+		publicPath: config.output.publicPath,
+		contentBase: 'src',
+		stats: {
+			colors: true,
+			hash: false,
+			timings: true,
+			chunks: false,
+			chunkModules: false,
+			modules: false
 		}
-	)
-);
+	});
 
-app.use(webpackHotMiddleware(compiler));
+	app.use(middleware);
+	app.use(webpackHotMiddleware(compiler));
+	app.get('*', function response(req, res) {
+		res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+		res.end();
+	});
+} else {
+	app.use(express.static(__dirname + '/dist'));
+	app.get('*', function response(req, res) {
+		res.sendFile(path.join(__dirname, 'dist/index.html'));
+	});
+}
 
-var pathToPublicFolder = path.join(__dirname, '../', webpackConfig.output.publicPath);
-
-app.use(function(req, res) {
-	res.sendFile(pathToPublicFolder + '/index.html');
-});
-
-app.listen(PORT, function(error) {
-	if (error) {
-		console.error(error);
-	} else {
-		console.info('Listening on http://localhost:%s', PORT);
+app.listen(port, '0.0.0.0', function onStart(err) {
+	if (err) {
+		console.log(err);
 	}
+	console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var webpackConfig = {
-	devtool: 'cheap-module-eval-source-map',
+module.exports = {
+	devtool: 'eval-source-map',
 	entry: [
-		'webpack-hot-middleware/client',
-		'./src/index.js'
+		'webpack-hot-middleware/client?reload=true',
+		path.join(__dirname, 'app/main.js')
 	],
 	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js',
-		publicPath: '/public/'
+		path: path.join(__dirname, '/dist/'),
+		filename: '[name].js',
+		publicPath: '/'
 	},
 	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'app/index.tpl.html',
+			inject: 'body',
+			filename: 'index.html'
+		}),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		})
 	],
 	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loaders: ['babel'],
-				exclude: /node_modules/,
-				include: __dirname
-
-			}
-		]
+		loaders: [{
+			test: /\.js?$/,
+			exclude: /node_modules/,
+			loader: 'babel'
+		}, {
+			test: /\.json?$/,
+			loader: 'json'
+		}, {
+			test: /\.css$/,
+			loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+		}]
 	}
 };
-
-module.exports = webpackConfig;
