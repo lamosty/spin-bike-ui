3dc712750e95660a7cefa8bfbbccc6e57825579e
Rastislav Lamos <lamos.rasto@gmail.com>
2016-03-01 22:12:00 +0100

    Remove Redux dev-tools (dock/log monitor)
    Let's use Chrome extension instead.

--- app/containers/Root.dev.js
+++ app/containers/Root.dev.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import routes from '../routes';
-import DevTools from './DevTools';
 import { Router, browserHistory } from 'react-router';
 
 export default class Root extends Component {
@@ -10,10 +9,7 @@ export default class Root extends Component {
 
 		return (
 			<Provider store={store}>
-				<div>
-					<Router history={browserHistory} routes={routes} />
-					<DevTools />
-				</div>
+				<Router history={browserHistory} routes={routes} />
 			</Provider>
 		);
 	}
