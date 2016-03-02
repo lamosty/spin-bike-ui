7a65958e3d290d75a6d41383445e563dee4652c8
Rastislav Lamos <lamos.rasto@gmail.com>
2016-03-02 21:18:30 +0100

    Remove excessive Root.[dev/prod].js components
    We have removed the redux-devtools (using Chrome extension now) so it is
    no longer needed to have two separate Root.js components for dev/prod.

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,5 +1,20 @@
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./Root.prod');
-} else {
-	module.exports = require('./Root.dev');
+import React, { Component, PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import routes from '../routes';
+import { Router, browserHistory } from 'react-router';
+
+export default class Root extends Component {
+	render() {
+		const { store } = this.props;
+
+		return (
+			<Provider store={store}>
+				<Router history={browserHistory} routes={routes} />
+			</Provider>
+		);
+	}
 }
+
+Root.propTypes = {
+	store: PropTypes.object.isRequired
+};
